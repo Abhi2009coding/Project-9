d1f680a430ac53e7acf1dfec36d37277a532e10b
Abhi2009coding <98991944+Abhi2009coding@users.noreply.github.com>
2022-02-24 17:35:54 -0600

    Add files via upload

---
+++ p5play_template-main/sketch.js
@@ -0,0 +1,22 @@
+var box1
+
+function setup() {
+  createCanvas(400,400);
+  box1 = createSprite(200,200,30,30)
+}
+
+function draw() 
+{
+  background(30);
+
+  if(keyIsDown(RIGHT_ARROW))   {
+    box1.position.x = box1.position.x + 5
+  }
+
+  if(keyIsDown(LEFT_ARROW))   {
+    box1.position.x = box1.position.x - 5
+  }
+
+  drawSprites()
+}
+
